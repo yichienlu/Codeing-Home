24716f255b2a117ab6df0d55d86d0effaa967fd6
yichienlu <yichien.lu89@gmail.com>
2022-08-24 15:01:02 +0800

    Update 2022-08-24T07:00:59.960Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,32 +1,34 @@
+"use strict";
+
 var shareSwiper = new Swiper('.shareSwiper', {
   effect: "fade",
   loop: true,
-
   // 分頁   
   pagination: {
     el: '.share-swiper-pagination',
-    clickable: true,
+    clickable: true
   },
   // 左右箭頭    
   navigation: {
     nextEl: '.share-swiper-button-next',
-    prevEl: '.share-swiper-button-prev',
+    prevEl: '.share-swiper-button-prev'
   },
   // 滾動條
   scrollbar: {
     el: '.swiper-scrollbar',
-    draggable: true,
-  },
+    draggable: true
+  }
 });
 var coursesSwiper = new Swiper(".coursesSwiper", {
   slidesPerView: "auto",
   spaceBetween: 16,
   freeMode: true,
-  loop: true,
+  loop: true
 });
 var categoriesSwiper = new Swiper(".categoriesSwiper", {
   slidesPerView: "auto",
   spaceBetween: 16,
   freeMode: true,
-  loop: true,
+  loop: true
 });
+//# sourceMappingURL=all.js.map
